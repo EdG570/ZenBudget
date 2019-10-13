4c819e8ea4097baea98cae2a63a45ab5e1a8111f
EdG570 <cole570@hotmail.com>
2019-10-13 09:51:17 -0400

    Add project files.

---
+++ BudgetTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BudgetTracker.Startup))]
+namespace BudgetTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
