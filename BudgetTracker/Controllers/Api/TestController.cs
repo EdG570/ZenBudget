6c086e17ce757da0d340a9604ff3ab5ffc7e395d
EdG570 <cole570@hotmail.com>
2019-10-13 10:20:26 -0400

    Initial Commit. Dependencies Added.

---
+++ BudgetTracker/Controllers/Api/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BudgetTracker.Controllers.Api
+{
+    public class TestController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
